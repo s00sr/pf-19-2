f026547a5c9148405a1d2a069ffce4069ba686f4
MiguelMottaVilca <46751756+MiguelMottaVilca@users.noreply.github.com>
2019-11-24 17:08:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
-#include "persona.h"
+#include "time.h"
 #include "usuario.h"
+#include "persona.h"
+#include "medico.h"
+#include "paciente.h"
+
 
 using namespace std;
 
@@ -13,7 +17,6 @@ void agregarUsuario( void ){
     ostringstream aux;
     string text;
     Usuario x;
-
 
     aux << x.getApellido() << "***" << x.getNombre() << "***" << x.getDni() << "***" << x.getUsuario() << "***" << x.getContra() ;
 
@@ -29,6 +32,80 @@ void agregarUsuario( void ){
 
     archivo.close();
 }
+
+void agregarPaciente(void){
+
+    ostringstream aux;
+    string text;
+    Usuario x;
+
+    Paciente tem();
+
+    aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getRegistro()  << "*****" ;
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("pacientes.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<endl;
+
+    archivo<< text;
+
+    archivo.close();
+
+}
+
+void agregarMedico(void){
+
+    ostringstream aux;
+    string text;
+    Usuario x;
+
+    Medico tem();
+
+    aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getRegistro()  << "*****" ;
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("medicos.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<endl;
+
+    archivo<< text;
+
+    archivo.close();
+
+}
+
+void agregarEnfermeros(void){
+
+    ostringstream aux;
+    string text;
+    Usuario x;
+
+    Enfermeros tem();
+
+    aux << tem.getApellido() << "___" << tem.getNombre() << "___" << tem.getDni() << "___" << tem.getRegistro()  << "*****" ;
+
+    text = aux.str();
+
+    ofstream archivo;
+
+    archivo.open("enfermeros.txt",ios::app);
+    if (archivo.fail())
+        std::cout<<"No se puedo abrir el archivo"<<endl;
+
+    archivo<< text;
+
+    archivo.close();
+
+}
+
+
 
 void confirmar( string usuario , string contra ){
 
@@ -51,19 +128,19 @@ void confirmar( string usuario , string contra ){
 
 void inicio(void){
     int num;
-    std::cout << '\n' << "      1    INGRESAR COMO A       ";
-    std::cout << '\n' << "      1    INGRESAR COMO B       ";
-    std::cout << '\n' << "      1    INGRESAR COMO C       ";
-    std::cout << '\n' << "      1    INGRESAR COMO D       ";
+    std::cout << '\n' << "      1    INGRESAR COMO ASISTENTE         ";
+    std::cout << '\n' << "      2    INGRESAR COMO MEDICO           ";
+    std::cout << '\n' << "      3    INGRESAR COMO ENFERMERO         ";
+    std::cout << '\n' << "      1    INGRESAR COMO PACIENTE        ";
     std::cout << '\n' << "      0    SALIR                  " << '\n';
     std::cout << '\n' << "DIGITE NUMERO: " ; std::cin >> num;
     if ( num == 1){
         std::string usuario , contra;
-        system("CLS");
+        system("clear");
         std::cin.ignore();
         std::cout << '\n' << "      USUARIO : ";
         std::getline (std::cin , usuario);
-        std::cout << "      CONTRASEÑA : ";
+        std::cout << "      CONTRASEÃ‘A : ";
         std::getline (std::cin, contra);
         confirmar( usuario , contra );
 
@@ -76,18 +153,14 @@ void inicio(void){
     }else{
         std::cout << "DIGITE EN EL RANGO DE NUMEROS" <<'\n';
         num = 100;
-        system("CLS");
+        system("clear");
         inicio();
     }
-
 }
 
 int main(){
 
-    //inicio();
-    //confirmar("a" , "b");
-    agregarUsuario();
-    //Usuario x;
-    //Persona y;
+    inicio();
+
     return 0;
 }
